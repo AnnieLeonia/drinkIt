b5b51b884d0625f54271946ec3f5b1cb22aa9df2
Annie Önnered <annie.onnered@gmail.com>
2017-09-17 18:25:32 +0200

    Added public folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./connect.js');
 const express = require('express');
 
 const app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('App is running.');
