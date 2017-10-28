bcf9cc1ab1737e7f161c4d99611ad38cfb653a9a
Annie Önnered <annie.onnered@gmail.com>
2017-10-28 16:22:29 +0200

    Connected database to user interface

--- connect.js
+++ connect.js
@@ -2,11 +2,9 @@ var mysql      = require('mysql');
 require('dotenv').config();
 var connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.connect();
-
 connection.query('show tables', function (error, results, fields) {
   if (error) throw error;
   console.log(results);
 });
 
-connection.end();
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-require('./connect.js');
-
+const conn    = require('./connect.js');
 const express = require('express');
+const app     = express();
 
-const app = express();
 app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
+
   res.render('homepage', { title: 'Homepage' });
+});
+
+app.get('/drinks', function(req, res){
+  conn.query('select name from recipes', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+
+    res.render('drinks', { title: 'Drinks', drinks: results});
+  });
 });
 
 app.listen(process.env.PORT || 8080, function () {
