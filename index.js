b6a9bed3e3ed760938ba785f2c6620858ca02c0c
Annie Önnered <annie.onnered@gmail.com>
2017-10-28 15:23:10 +0200

    Added views and modified express settings

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require('./connect.js');
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  res.render('homepage', { title: 'Homepage' });
+});
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('App is running.');
