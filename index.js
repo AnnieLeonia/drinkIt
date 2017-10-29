448c1d5f58be57d9a01cc41b9d1b200f561e9844
Annie Önnered <annie.onnered@gmail.com>
2017-10-29 18:21:54 +0100

    Added functionality to create drinks, added bodyparser and a head-file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const conn    = require('./connect.js');
-const express = require('express');
-const app     = express();
+const conn        = require('./connect.js');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
 
@@ -19,11 +22,38 @@ app.get('/drinks', function(req, res){
 });
 
 app.get('/drinks/:name', function(req, res){
-  const drinkName = req.params.name;
-  conn.query('SELECT ingredient, amount FROM recipeitems WHERE recipe = "' + drinkName + '"', function(error, recipeitems){
+  conn.query('SELECT name, description FROM recipes WHERE name = ?', [req.params.name],
+    function(error, recipes){
+      if(recipes.length > 0){
+        const drinkName = recipes[0].name;
+        const drinkDescription = recipes[0].description;
+        conn.query('SELECT ingredient, amount FROM recipeitems WHERE recipe = ?', [drinkName],
+          function(error, recipeitems){
+            res.render('drink', { title: drinkName + ' - Drink.It', drinkName, recipeitems});
+          }
+        );
+      } else {
+        res.send("Sorry, " + req.params.name + " does not exist");
+      }
+    });
+});
 
-    res.render('drink', { title: drinkName + ' - Drink.It', drinkName, recipeitems});
-  });
+app.get('/newDrink', function(req, res){
+  res.render('newDrink', {title:'New Drink - Drink.It'})
+});
+
+app.post('/newDrink', function(req, res){
+  const drinkName = req.body.name;
+  const drinkDescription =  req.body.description;
+  conn.query('INSERT INTO recipes(name, description) values (?, ?)',
+    [drinkName,drinkDescription], function(error){
+      if(error==null){
+        res.redirect('/drinks/' + drinkName);
+      } else {
+        console.log(error);
+        res.send("Sorry, " + drinkName + " could not be created!");
+      }
+    });
 });
 
 app.listen(process.env.PORT || 8080, function () {
